ef0bd336cdb4b4cfd137991f09f6f9027695fea0
salehkamalkamel <115103594+salehkamalkamel@users.noreply.github.com>
2024-08-31 23:55:35 +0300

    mobile menu

--- src/ui/MobileMenu.jsx
+++ src/ui/MobileMenu.jsx
@@ -33,6 +33,21 @@ export default function MobileMenu() {
   }
 
   useEffect(() => {
+    if (isMenuOpen) {
+      // Disable background scrolling when menu is open
+      document.body.style.overflow = 'hidden';
+    } else {
+      // Re-enable background scrolling when menu is closed
+      document.body.style.overflow = '';
+    }
+
+    // Cleanup function to re-enable scrolling in case component unmounts
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isMenuOpen]);
+
+  useEffect(() => {
     setIsMenuOpen(false);
   }, [pathname]);
 
@@ -56,17 +71,19 @@ export default function MobileMenu() {
             className="fixed inset-0 bg-black opacity-30 z-40"
             onClick={handleToggleMenu}
           ></div>
-          <div className="absolute top-[5.5rem] left-0 right-0 bg-white rounded-br-lg rounded-bl-lg grid mobile:grid-cols-1 grid-cols-3 px-4 py-14 gap-y-24 gap-x-4 z-50">
-            {menuItems.map(({ imageSrc, altText, label, path }, index) => (
-              <MobileMenuItem
-                key={index}
-                imageSrc={imageSrc}
-                altText={altText}
-                label={label}
-                path={path}
-                onClick={handleToggleMenu}
-              />
-            ))}
+          <div className="fixed top-[5.5rem] left-0 right-0 bottom-0 bg-white rounded-br-lg rounded-bl-lg overflow-y-auto z-50 mb-24">
+            <div className="px-4 py-14 grid mobile:grid-cols-1 grid-cols-3 gap-y-24 gap-x-4">
+              {menuItems.map(({ imageSrc, altText, label, path }, index) => (
+                <MobileMenuItem
+                  key={index}
+                  imageSrc={imageSrc}
+                  altText={altText}
+                  label={label}
+                  path={path}
+                  onClick={handleToggleMenu}
+                />
+              ))}
+            </div>
           </div>
         </>
       )}
